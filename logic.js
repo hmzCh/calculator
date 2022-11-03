9af808852bb4062b96bc028f4bd3547d06a8e511
Hamza <114156862+hmzCh@users.noreply.github.com>
2022-11-03 17:35:40 +0000

    Add functions
    Forgot to commit this file at the start so it's been through a few iterations, none of which have been recorded

---
+++ logic.js
@@ -0,0 +1,37 @@
+const primaryDisplay = document.querySelector("#primaryDisplay")
+const secondaryDisplay = document.querySelector("#secondaryDisplay")
+let equation = ""
+
+const numPad = Array.from(document.querySelectorAll(".numPad"))
+numPad.forEach(button => {
+    button.addEventListener("click", () => addToDisplay(button.textContent))
+}); 
+function addToDisplay(input) {
+    equation = equation + input
+    primaryDisplay.textContent = equation;
+};
+
+const operators = Array.from(document.querySelectorAll(".operators"))
+operators.forEach(button => {
+    button.addEventListener("click", () => operatorClicked(button.textContent))
+})
+
+function operatorClicked(input) {
+    equation = equation + input
+    secondaryDisplay.textContent = equation
+}
+
+const clearButton = document.querySelector("#clearButton")
+clearButton.addEventListener("click", () => clearDisplay())
+function clearDisplay() {
+    equation = ""
+    primaryDisplay.textContent = "0"
+}
+
+const backButton = document.querySelector("#backButton")
+backButton.addEventListener("click", () => removeLastCharacter())
+
+function removeLastCharacter() {
+    equation = equation.slice(0, -1)
+    primaryDisplay.textContent = equation
+}
